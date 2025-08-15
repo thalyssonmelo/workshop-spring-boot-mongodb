5a15fd74aacd46015de299312ca430d1e6785415
Thalysson Melo <thalyssonmelo840@gmail.com>
2025-08-15 15:12:31 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/thalyssonmelo/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.thalyssonmelo.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.thalyssonmelo.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
