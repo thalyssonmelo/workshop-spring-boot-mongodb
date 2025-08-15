cd33fe1f502af23e3534b0af94b08f5d40be13c4
Thalysson Melo <thalyssonmelo840@gmail.com>
2025-08-15 15:49:40 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/thalyssonmelo/workshopmongo/resources/UserResource.java
+++ src/main/java/com/thalyssonmelo/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.thalyssonmelo.workshopmongo.domain.Post;
 import com.thalyssonmelo.workshopmongo.domain.User;
 import com.thalyssonmelo.workshopmongo.dto.UserDTO;
 import com.thalyssonmelo.workshopmongo.services.UserService;
@@ -60,6 +61,12 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value="/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPost());
 	}
 
 }
